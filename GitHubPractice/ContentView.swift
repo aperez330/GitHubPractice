60b2b6d4272512585194ba9f6d0eac89b1710f52
vivian jelinek <viviancjelinek@gmail.com>
2023-07-18 10:03:34 -0500

    made change

--- GitHubPractice/ContentView.swift
+++ GitHubPractice/ContentView.swift
@@ -13,7 +13,10 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Bye!, Bye!")
+            Text("Bye, Bye!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.pink)
         }
         .padding()
     }
