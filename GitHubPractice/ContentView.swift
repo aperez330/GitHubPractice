145e31c96e016cb04fdf6c6cd35935c8a4abdc22
Alexandra <alexandraperez3419@gmail.com>
2023-07-18 09:49:44 -0500

    djjdjdjdd

--- GitHubPractice/ContentView.swift
+++ GitHubPractice/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Bye, Byes!")
+            Text("Bye Bye!")
         }
         .padding()
     }
