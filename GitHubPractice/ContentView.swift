90b5bd6bdce4d23205e34267cee118b496f95dc8
Alexandra <alexandraperez3419@gmail.com>
2023-07-18 09:40:51 -0500

    Initial Commit

---
+++ GitHubPractice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubPractice
+//
+//  Created by scholar on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Bye Bye")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubPractice/GitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticeApp.swift
+//  GitHubPractice
+//
+//  Created by scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
